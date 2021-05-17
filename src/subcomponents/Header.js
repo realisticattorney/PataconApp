15b9c11821d591bb6bf747d97eaf52922a923ac7
German Aquila <g.axl.aquila@gmail.com>
2021-05-16 21:34:02 -0300

    Add shadows they're a GAME CHANGER

--- src/subcomponents/Header.js
+++ src/subcomponents/Header.js
@@ -24,23 +24,25 @@ const Header = () => {
   return (
     <div className="shadow">
       <Nav />
-      <header className="header">
-        <div className="header__text-box">
-          <h1 className="heading-primary">
-            <span className="heading-primary--main">1 eth</span>
-            <span className="heading-primary--sub">= 1 peso</span>
-          </h1>
-          <div class="frame">
-            <button
-              class="custom-btn btn-7 test1"
-              to="test1"
-              onClick={() => scrollTo()}
-            >
-              <span>Get MNM today</span>
-            </button>
+      <div className="shadoww">
+        <header className="header">
+          <div className="header__text-box">
+            <h1 className="heading-primary">
+              <span className="heading-primary--main">1 eth</span>
+              <span className="heading-primary--sub">= 1 peso</span>
+            </h1>
+            <div class="frame">
+              <button
+                class="custom-btn btn-7 test1"
+                to="test1"
+                onClick={() => scrollTo()}
+              >
+                <span>Get MNM today</span>
+              </button>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
+      </div>
       <div name="scroll-to-element" className="element">
         <App />
       </div>
